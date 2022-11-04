LairdCP/Attributes

commit fe7d61535f8f2aed86c0028338de24833a215bb5

New key-value pair format
Bug 22438
Use names instead of IDs in key-value file and allow comments.
Deprecate lock and dump display options.
Build feedback file in chunks.
Rename script and output to api.

Bug 20686
Generate max file size estimate
Generate default file

Signed-off-by: Andrew Hedin <andrew.hedin@lairdconnect.com>

include/attr.h
@@ -442,9 +442,8 @@ int attr_set_mask32(attr_id_t id, uint8_t bit, uint8_t value);
  */
 int attr_set_mask64(attr_id_t id, uint8_t bit, uint8_t value);
 
-#ifdef CONFIG_ATTR_SHELL
 /**
- * @brief Get the id of an attribute
+ * @brief Get the id (table index) of an attribute using the name
  *
  * @param name of the attribute
  *
@@ -493,7 +492,6 @@ int attr_delete(void);
  */
 const char *const attr_get_string_set_error(int value);
 
-#endif /* CONFIG_ATTR_SHELL */
 
 /**
  * @brief Print all parameters to the console using system workq.  The prepare
@@ -529,7 +527,7 @@ int attr_set_quiet(attr_id_t id, bool value);
  *
  * @retval negative error code, number of parameters on success
  */
-int attr_load(const char *abs_path, const char *feedback_path, bool *modified);
+int attr_load(const char *abs_path, bool *modified);
 
 /**
  * @brief Notification callback

src/attribute_mgmt.c
@@ -439,9 +439,12 @@ static int set_parameter(struct mgmt_ctxt *ctxt)
 	struct set_parameter user_params;
 	struct set_parameter_result response;
 	enum attr_type type;
+	bool *mod_ptr = NULL;
 #ifdef CONFIG_ATTRIBUTE_MGMT_INCREMENT_CONFIG_VERSION
 	bool modified = false;
+	mod_ptr = &modified;
 #endif
+
 	int r = 0;
 	zcbor_state_t *zse = ctxt->cnbe->zs;
 	zcbor_state_t *zsd = ctxt->cnbd->zs;
@@ -465,13 +468,7 @@ static int set_parameter(struct mgmt_ctxt *ctxt)
 		    _set_parameter_p2_bool) {
 			r = attr_set((attr_id_t)user_params._set_parameter_p1,
 				     type, &user_params._set_parameter_p2_bool,
-				     sizeof(bool),
-#ifdef CONFIG_ATTRIBUTE_MGMT_INCREMENT_CONFIG_VERSION
-				     modified
-#else
-				     NULL
-#endif
-			);
+				     sizeof(bool), mod_ptr);
 		} else {
 			r = -ENOMSG;
 		}
@@ -504,12 +501,7 @@ static int set_parameter(struct mgmt_ctxt *ctxt)
 				r = attr_set(
 					(attr_id_t)user_params._set_parameter_p1,
 					type, &tmp_val, sizeof(int8_t),
-#ifdef CONFIG_ATTRIBUTE_MGMT_INCREMENT_CONFIG_VERSION
-					modified
-#else
-					NULL
-#endif
-				);
+					mod_ptr);
 			} else if (type == ATTR_TYPE_S16) {
 				int16_t tmp_val =
 					(int16_t)user_params
@@ -517,12 +509,7 @@ static int set_parameter(struct mgmt_ctxt *ctxt)
 				r = attr_set(
 					(attr_id_t)user_params._set_parameter_p1,
 					type, &tmp_val, sizeof(int16_t),
-#ifdef CONFIG_ATTRIBUTE_MGMT_INCREMENT_CONFIG_VERSION
-					modified
-#else
-					NULL
-#endif
-				);
+					mod_ptr);
 			} else if (type == ATTR_TYPE_S32) {
 				int32_t tmp_val =
 					(int32_t)user_params
@@ -530,24 +517,13 @@ static int set_parameter(struct mgmt_ctxt *ctxt)
 				r = attr_set(
 					(attr_id_t)user_params._set_parameter_p1,
 					type, &tmp_val, sizeof(int32_t),
-#ifdef CONFIG_ATTRIBUTE_MGMT_INCREMENT_CONFIG_VERSION
-					modified
-#else
-					NULL
-#endif
-				);
+					mod_ptr);
 			} else if (type == ATTR_TYPE_S64) {
 				r = attr_set(
 					(attr_id_t)user_params._set_parameter_p1,
 					type,
 					&user_params._set_parameter_p2__int64,
-					sizeof(int64_t),
-#ifdef CONFIG_ATTRIBUTE_MGMT_INCREMENT_CONFIG_VERSION
-					modified
-#else
-					NULL
-#endif
-				);
+					sizeof(int64_t), mod_ptr);
 			}
 		} else {
 			r = -ENOMSG;
@@ -576,12 +552,7 @@ static int set_parameter(struct mgmt_ctxt *ctxt)
 				r = attr_set(
 					(attr_id_t)user_params._set_parameter_p1,
 					type, &tmp_val, sizeof(uint8_t),
-#ifdef CONFIG_ATTRIBUTE_MGMT_INCREMENT_CONFIG_VERSION
-					modified
-#else
-					NULL
-#endif
-				);
+					mod_ptr);
 			} else if (type == ATTR_TYPE_U16) {
 				uint16_t tmp_val =
 					(uint16_t)user_params
@@ -589,12 +560,7 @@ static int set_parameter(struct mgmt_ctxt *ctxt)
 				r = attr_set(
 					(attr_id_t)user_params._set_parameter_p1,
 					type, &tmp_val, sizeof(uint16_t),
-#ifdef CONFIG_ATTRIBUTE_MGMT_INCREMENT_CONFIG_VERSION
-					modified
-#else
-					NULL
-#endif
-				);
+					mod_ptr);
 			} else if (type == ATTR_TYPE_U32) {
 				uint32_t tmp_val =
 					(uint32_t)user_params
@@ -602,24 +568,13 @@ static int set_parameter(struct mgmt_ctxt *ctxt)
 				r = attr_set(
 					(attr_id_t)user_params._set_parameter_p1,
 					type, &tmp_val, sizeof(uint32_t),
-#ifdef CONFIG_ATTRIBUTE_MGMT_INCREMENT_CONFIG_VERSION
-					modified
-#else
-					NULL
-#endif
-				);
+					mod_ptr);
 			} else if (type == ATTR_TYPE_U64) {
 				r = attr_set(
 					(attr_id_t)user_params._set_parameter_p1,
 					type,
 					&user_params._set_parameter_p2__uint64,
-					sizeof(uint64_t),
-#ifdef CONFIG_ATTRIBUTE_MGMT_INCREMENT_CONFIG_VERSION
-					modified
-#else
-					NULL
-#endif
-				);
+					sizeof(uint64_t), mod_ptr);
 			}
 		} else {
 			r = -ENOMSG;
@@ -631,12 +586,7 @@ static int set_parameter(struct mgmt_ctxt *ctxt)
 				(attr_id_t)user_params._set_parameter_p1, type,
 				(void *)user_params._set_parameter_p2_tstr.value,
 				user_params._set_parameter_p2_tstr.len,
-#ifdef CONFIG_ATTRIBUTE_MGMT_INCREMENT_CONFIG_VERSION
-				modified
-#else
-				NULL
-#endif
-			);
+				mod_ptr);
 		} else {
 			r = -ENOMSG;
 		}
@@ -646,13 +596,7 @@ static int set_parameter(struct mgmt_ctxt *ctxt)
 			float tmp_val =
 				(float)user_params._set_parameter_p2_float;
 			r = attr_set((attr_id_t)user_params._set_parameter_p1,
-				     type, &tmp_val, sizeof(float),
-#ifdef CONFIG_ATTRIBUTE_MGMT_INCREMENT_CONFIG_VERSION
-				     modified
-#else
-				     NULL
-#endif
-			);
+				     type, &tmp_val, sizeof(float), mod_ptr);
 		} else {
 			r = -ENOMSG;
 		}
@@ -663,12 +607,7 @@ static int set_parameter(struct mgmt_ctxt *ctxt)
 				(attr_id_t)user_params._set_parameter_p1, type,
 				(void *)user_params._set_parameter_p2_bstr.value,
 				user_params._set_parameter_p2_bstr.len,
-#ifdef CONFIG_ATTRIBUTE_MGMT_INCREMENT_CONFIG_VERSION
-				modified
-#else
-				NULL
-#endif
-			);
+				mod_ptr);
 		} else {
 			r = -ENOMSG;
 		}
@@ -714,11 +653,6 @@ static int load_parameter_file(struct mgmt_ctxt *ctxt)
 		return MGMT_ERR_EINVAL;
 	}
 
-#ifdef CONFIG_ATTR_SETTINGS_LOCK
-	if (attr_is_locked() == true) {
-		r = -EACCES;
-	}
-#endif
 #ifdef ATTR_ID_load_path
 	load_path = attr_get_quasi_static(ATTR_ID_load_path);
 #else
@@ -729,39 +663,13 @@ static int load_parameter_file(struct mgmt_ctxt *ctxt)
 		/* The input file is an optional parameter. */
 		r = attr_load((user_params._load_parameter_file_p1_present ==
 					       true ?
-				       user_params._load_parameter_file_p1
+					     user_params._load_parameter_file_p1
 					       ._load_parameter_file_p1.value :
-				       (void *)load_path),
-#ifdef CONFIG_ATTR_LOAD_FEEDBACK
-			      CONFIG_ATTRIBUTE_MGMT_FEEDBACK_FILE,
-#else
-			      NULL,
-#endif
+					     (void *)load_path),
 			      &modified);
 	}
 
 	load_parameter_file_data._load_parameter_file_result_r = r;
-
-	if (r == 0) {
-#ifdef CONFIG_ATTR_LOAD_FEEDBACK
-		/* Encode the feedback file path. */
-		load_parameter_file_data._load_parameter_file_result_f
-			._load_parameter_file_result_f.value =
-			CONFIG_ATTRIBUTE_MGMT_FEEDBACK_FILE;
-		load_parameter_file_data._load_parameter_file_result_f
-			._load_parameter_file_result_f.len =
-			strlen(CONFIG_ATTRIBUTE_MGMT_FEEDBACK_FILE);
-		load_parameter_file_data._load_parameter_file_result_f_present =
-			true;
-
-#ifdef CONFIG_ATTR_CONFIGURATION_VERSION
-		/* If modified, update the device configuration version */
-		if (modified == true) {
-			attr_update_config_version();
-		}
-#endif
-#endif
-	}
 
 	if (!cbor_encode_load_parameter_file_result(
 		    zse->payload_mut, (zse->payload_end - zse->payload),
@@ -790,12 +698,6 @@ static int dump_parameter_file(struct mgmt_ctxt *ctxt)
 					     &user_params, NULL)) {
 		return MGMT_ERR_EINVAL;
 	}
-
-#ifdef CONFIG_ATTR_SETTINGS_LOCK
-	if (attr_is_locked() == true) {
-		r = -EACCES;
-	}
-#endif
 
 	if (r == 0) {
 		file_name = NULL;
@@ -866,12 +768,6 @@ static int factory_reset(struct mgmt_ctxt *ctxt)
 	uint8_t factory_reset_enabled = 0;
 #endif
 	zcbor_state_t *zse = ctxt->cnbe->zs;
-
-#ifdef CONFIG_ATTR_SETTINGS_LOCK
-	if (attr_is_locked() == true) {
-		r = -EACCES;
-	}
-#endif
 
 	if (r == 0) {
 #ifdef ATTR_ID_factory_reset_enable
@@ -989,254 +885,27 @@ static int disable_notify(struct mgmt_ctxt *ctxt)
 
 static int check_lock_status(struct mgmt_ctxt *ctxt)
 {
-#ifdef CONFIG_ATTR_SETTINGS_LOCK
-	uint32_t rsp_len = 0;
-	struct check_lock_status_result check_lock_status_data;
-	bool lock_enabled;
-	bool lock_active = false;
-	uint8_t lock_status;
-	int r = 0;
-	zcbor_state_t *zse = ctxt->cnbe->zs;
-
-	r = attr_get(ATTR_ID_lock, &lock_enabled, sizeof(lock_enabled));
-
-	if (r >= 0) {
-		r = attr_get(ATTR_ID_lock_status, &lock_status,
-			     sizeof(lock_status));
-
-		if (r >= 0) {
-			if (lock_status == LOCK_STATUS_SETUP_ENGAGED) {
-				lock_active = true;
-			}
-
-			/* Completed successfully so return success result
-			 * code
-			 */
-			r = 0;
-		}
-	}
-
-	check_lock_status_data._check_lock_status_result_r = r;
-	check_lock_status_data._check_lock_status_result_r1 = lock_enabled;
-	check_lock_status_data._check_lock_status_result_r2 = lock_active;
-
-	if (!cbor_encode_check_lock_status_result(
-		    zse->payload_mut, (zse->payload_end - zse->payload),
-		    &check_lock_status_data, &rsp_len)) {
-		return MGMT_ERR_EMSGSIZE;
-	}
-
-	zse->payload += rsp_len;
-
-	return MGMT_ERR_EOK;
-#else
 	return MGMT_ERR_ENOTSUP;
-#endif
 }
 
 static int set_lock_code(struct mgmt_ctxt *ctxt)
 {
-#ifdef CONFIG_ATTR_SETTINGS_LOCK
-	uint32_t rsp_len = 0;
-	struct set_lock_code user_params;
-	struct set_lock_code_result set_lock_code_data;
-	int r = 0;
-	zcbor_state_t *zse = ctxt->cnbe->zs;
-	zcbor_state_t *zsd = ctxt->cnbd->zs;
-
-	if (!cbor_decode_set_lock_code(zsd->payload,
-				       (zsd->payload_end - zsd->payload),
-				       &user_params, NULL)) {
-		return MGMT_ERR_EINVAL;
-	}
-
-	if (attr_is_locked() == true) {
-		r = -EACCES;
-	}
-
-	if (r == 0) {
-		r = attr_set_uint32(ATTR_ID_settings_passcode,
-				    user_params._set_lock_code_p1);
-	}
-
-	if (r == 0) {
-		r = attr_set_uint32(ATTR_ID_lock, true);
-	}
-
-	if (r == 0) {
-		/* This sets the lock ready to be engaged when the user
-		 * disconnects, when the module is rebooted or when the user
-		 * manually requests it with the lock command, but allows
-		 * further configuration changes to the unit until then
-		 */
-		r = attr_set_uint32(ATTR_ID_lock_status,
-				    LOCK_STATUS_SETUP_DISENGAGED);
-
-		ATTR_FRAMEWORK_BROADCAST(FMC_ATTR_LOCK_STATUS_CHANGED);
-	}
-
-	set_lock_code_data._set_lock_code_result_r = r;
-
-	if (!cbor_encode_set_lock_code_result(zse->payload_mut,
-					      (zse->payload_end - zse->payload),
-					      &set_lock_code_data, &rsp_len)) {
-		return MGMT_ERR_EMSGSIZE;
-	}
-
-	zse->payload += rsp_len;
-
-	return MGMT_ERR_EOK;
-#else
 	return MGMT_ERR_ENOTSUP;
-#endif
 }
 
 static int lock(struct mgmt_ctxt *ctxt)
 {
-#ifdef CONFIG_ATTR_SETTINGS_LOCK
-	uint32_t rsp_len = 0;
-	struct lock_result lock_data;
-	enum settings_passcode_status passcode_status =
-		SETTINGS_PASSCODE_STATUS_UNDEFINED;
-	int r = 0;
-	zcbor_state_t *zse = ctxt->cnbe->zs;
-
-	if (attr_is_locked() == false) {
-		/* Lock the settings */
-		attr_set_uint32(ATTR_ID_lock, true);
-		attr_set_uint32(ATTR_ID_lock_status, LOCK_STATUS_SETUP_ENGAGED);
-		passcode_status = SETTINGS_PASSCODE_STATUS_VALID_CODE;
-
-		/* Send feedback about the passcode */
-		attr_set_uint32(ATTR_ID_settings_passcode_status,
-				passcode_status);
-
-		ATTR_FRAMEWORK_BROADCAST(FMC_ATTR_LOCK_STATUS_CHANGED);
-	}
-
-	lock_data._lock_result_r = r;
-
-	if (!cbor_encode_lock_result(zse->payload_mut,
-				     (zse->payload_end - zse->payload),
-				     &lock_data, &rsp_len)) {
-		return MGMT_ERR_EMSGSIZE;
-	}
-
-	zse->payload += rsp_len;
-
-	return MGMT_ERR_EOK;
-#else
 	return MGMT_ERR_ENOTSUP;
-#endif
 }
 
 static int unlock(struct mgmt_ctxt *ctxt)
 {
-#ifdef CONFIG_ATTR_SETTINGS_LOCK
-	uint32_t rsp_len = 0;
-	enum settings_passcode_status passcode_status =
-		SETTINGS_PASSCODE_STATUS_UNDEFINED;
-	struct unlock user_params;
-	struct unlock_result unlock_data;
-	uint32_t real_lock_code;
-	int r = 0;
-	zcbor_state_t *zse = ctxt->cnbe->zs;
-	zcbor_state_t *zsd = ctxt->cnbd->zs;
-
-	if (!cbor_decode_unlock(zsd->payload, (zsd->payload_end - zsd->payload),
-				&user_params, NULL)) {
-		return MGMT_ERR_EINVAL;
-	}
-
-	if (attr_is_locked() == true) {
-		real_lock_code =
-			attr_get_uint32(ATTR_ID_settings_passcode, 123456);
-
-		/* Check if the passcode entered matches */
-		if (real_lock_code == user_params._unlock_p1) {
-			/* Unlock the settings */
-			attr_set_uint32(ATTR_ID_lock_status,
-					LOCK_STATUS_SETUP_DISENGAGED);
-			passcode_status = SETTINGS_PASSCODE_STATUS_VALID_CODE;
-		} else {
-			passcode_status = SETTINGS_PASSCODE_STATUS_INVALID_CODE;
-			r = -EINVAL;
-
-			/* Sleep for 1.5 seconds to slow down possible
-			 * attacks
-			 */
-			k_sleep(K_MSEC(LOCK_INVALID_WAIT_TIME_MS));
-		}
-
-		/* Send feedback to APP about the passcode */
-		attr_set_uint32(ATTR_ID_settings_passcode_status,
-				passcode_status);
-	}
-
-	if (user_params._unlock_p2 == true && attr_is_locked() == false &&
-	    r == 0) {
-		/* User has requested to remove the lock entirely */
-		attr_set_uint32(ATTR_ID_lock, false);
-		attr_set_uint32(ATTR_ID_lock_status, LOCK_STATUS_NOT_SETUP);
-	}
-
-	if (r == 0) {
-		ATTR_FRAMEWORK_BROADCAST(FMC_ATTR_LOCK_STATUS_CHANGED);
-	}
-
-	unlock_data._unlock_result_r = r;
-
-	if (!cbor_encode_unlock_result(zse->payload_mut,
-				       (zse->payload_end - zse->payload),
-				       &unlock_data, &rsp_len)) {
-		return MGMT_ERR_EMSGSIZE;
-	}
-
-	zse->payload += rsp_len;
-
-	return MGMT_ERR_EOK;
-#else
 	return MGMT_ERR_ENOTSUP;
-#endif
 }
 
 static int get_unlock_error_code(struct mgmt_ctxt *ctxt)
 {
-#ifdef CONFIG_ATTR_SETTINGS_LOCK
-	uint32_t rsp_len = 0;
-	struct get_unlock_error_code_result unlock_error_code_data;
-	enum settings_passcode_status passcode_status =
-		SETTINGS_PASSCODE_STATUS_UNDEFINED;
-	int r = 0;
-	zcbor_state_t *zse = ctxt->cnbe->zs;
-
-	r = attr_get(ATTR_ID_settings_passcode_status, &passcode_status,
-		     sizeof(passcode_status));
-
-	if (r >= 0) {
-		/* Clear status */
-		attr_set_uint32(ATTR_ID_settings_passcode_status,
-				SETTINGS_PASSCODE_STATUS_UNDEFINED);
-
-		r = 0;
-	}
-
-	unlock_error_code_data._get_unlock_error_code_result_r = r;
-	unlock_error_code_data._get_unlock_error_code_result_r1 =
-		passcode_status;
-
-	if (!cbor_encode_get_unlock_error_code_result(
-		    zse->payload_mut, (zse->payload_end - zse->payload),
-		    &unlock_error_code_data, &rsp_len)) {
-		return MGMT_ERR_EMSGSIZE;
-	}
-
-	zse->payload += rsp_len;
-
-	return MGMT_ERR_EOK;
-#else
 	return MGMT_ERR_ENOTSUP;
-#endif
 }
 
 static int get_api_version(struct mgmt_ctxt *ctxt)

CMakeLists.txt
@@ -28,10 +28,6 @@ zephyr_sources(
     ${ATTR_CUSTOM_PATH_BASE}/src/attr_table.c
 )
 
-if(CONFIG_ATTR_SETTINGS_LOCK)
-zephyr_sources_ifdef(CONFIG_ATTR_SHELL_LOCK src/shell_lock.c)
-endif()
-
 if(CONFIG_FRAMEWORK)
     # Add framework ID and msgcode files
     add_fwk_id_file(${CMAKE_CURRENT_SOURCE_DIR}/framework/attr_ids.h)

Kconfig
@@ -4,9 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 menuconfig ATTR
-	bool "Enable attribute/parameters module"
-	depends on FILE_SYSTEM_UTILITIES
-	depends on LCZ_PARAM_FILE
+	bool "Enable attributes module"
+	depends on LCZ_KVP
 
 if ATTR
 
@@ -19,9 +18,9 @@ config ATTR_BROADCAST
 config ATTR_INIT_PRIORITY
 	int "Init priority Application for attribute module"
 	range 2 99
-	default 2
+	default 70
 	help
-	  Can't be higher than LCZ_PARAM_FILE_INIT_PRIORITY.
+	  Can't be higher than LCZ_KVP_INIT_PRIORITY.
 	  Recommended to be higher than main thread init priority.
 
 config ATTR_FILE_NAME
@@ -30,13 +29,6 @@ config ATTR_FILE_NAME
 
 config ATTR_BREAK_ON_LOAD_FAILURE
 	bool "Stop loading on first failure"
-
-config ATTR_STRING_NAME
-	bool "Enable string name storage/retrieval"
-	default y
-	help
-	  Will allow storage and retrieval of attribute names, this will
-	  increase flash usage somewhat.
 
 config ATTR_SHELL
 	bool "Enable Attribute Shell"
@@ -49,14 +41,6 @@ config ATTR_SHELL_FACTORY_RESET_MSG
 	depends on FRAMEWORK
 	default y if ATTR_SHELL && FRAMEWORK
 
-config ATTR_SHELL_LOCK
-	bool "Enable settings lock shell manipulation"
-	depends on ATTR_SETTINGS_LOCK
-	default y if SHELL && ATTR_SETTINGS_LOCK
-	help
-	  Allows the configuration lock to be enabled, disabled, set,
-	  removed and checked via the 'lock' shell commands.
-
 config ATTR_SHELL_DATA_MANIPULATION
 	bool "Enable data manipilation functions"
 	help
@@ -66,19 +50,15 @@ config ATTR_SHELL_DATA_MANIPULATION
 	  Note: As this bypasses limitations placed on the attributes
 	        module, it should not be used for secure production code.
 
-config ATTR_SHELL_SETTINGS_MANIPULATION
-	bool "Enable settings manipulation functions"
+config ATTR_SHELL_NOTIFICATIONS
+	bool "Enable notification manipulation functions"
+	depends on CONFIG_MCUMGR_SMP_BT
 	help
-	  Allows shell functions that can be used to manipulate settings
-	  of attributes in the attribute module.
-
-	  Note: As this can bypass limitations placed on the attributes
-	        module, it should not be used for secure production code.
+	  Allows attributes notification to be turned on/off.
 
 choice ATTR_SHELL_CONFIG_MANIPULATION_MODE
 	prompt "Configuration manipulation"
-	default ATTR_SHELL_CONFIG_MANIPULATION_ALL if !ATTR_SHELL_LOCK
-	default ATTR_SHELL_CONFIG_MANIPULATION_LIMITED_WITH_LOCK if ATTR_SHELL_LOCK
+	default ATTR_SHELL_CONFIG_MANIPULATION_ALL
 	help
 	  Allows shell functions that can be used to manipulate
 	  configuration in the attribute module.
@@ -92,27 +72,12 @@ config ATTR_SHELL_CONFIG_MANIPULATION_ALL
 	  Note: As this can bypass limitations placed on the attributes
 	        module, it should not be used for secure production code.
 
-config ATTR_SHELL_CONFIG_MANIPULATION_ALL_WITH_LOCK
-	bool "All enabled (restricted with configuration lock)"
-	depends on ATTR_SHELL_LOCK
-	help
-	  Allows all attribute shell configuration manipulation
-	  functions, protected by the configuration lock.
-
 config ATTR_SHELL_CONFIG_MANIPULATION_LIMITED
 	bool "Limited functionality"
 	help
 	  Allows load and dump attribute shell configuration
 	  manipulation functions, other shell configuration functions
 	  are disabled.
-
-config ATTR_SHELL_CONFIG_MANIPULATION_LIMITED_WITH_LOCK
-	bool "Limited functionality (restricted with configuration lock)"
-	depends on ATTR_SHELL_LOCK
-	help
-	  Allows load and dump attribute shell configuration
-	  manipulation functions, protected by the configuration lock,
-	  other shell configuration functions are disable.
 
 config ATTR_SHELL_CONFIG_MANIPULATION_NONE
 	bool "Disabled"
@@ -168,31 +133,7 @@ config ATTR_SAVE_DELAY_MS
 	  change will be written back to the storage device (which will
 	  also include any additional changes up to the point of save).
 
-config ATTR_SAVE_STATUS_CALLBACK
-	bool "Save status callbacks"
-	help
-	  This will allow the application to be notified when the
-	  attributes are modified but not yet saved to the storage
-	  device or when saving to the storage device has completed.
-	  From the user application, attr_save_status_notification()
-	  needs to be implemented.
-
 endif # ATTR_DEFERRED_SAVE
-
-config ATTR_SETTINGS_LOCK
-	bool "Enable settings lock"
-	help
-	  The settings lock can be used to restrict altering module
-	  configuration/settings by protecting it with a 6-digit
-	  passcode. Once set and enabled, the settings passcode must be
-	  entered prior to making changes to configuration.
-
-config ATTR_CONFIGURATION_VERSION
-	bool "Configuration version attribute support"
-	help
-	  Adds support for the configuration version attribute which is
-	  used as a volatile value to indicate the version of the
-	  running configuration (reset on power cycle or reboot).
 
 config MCUMGR_CMD_ATTRIBUTE_MGMT
 	bool "Enable the attribute MCUMGR interface"
@@ -223,11 +164,10 @@ config MGMT_GROUP_ID_ATTRIBUTE
 	default 65
 
 config ATTRIBUTE_MGMT_INCREMENT_CONFIG_VERSION
-	bool "Increment config version from SMP set parameter"
-	depends on ATTR_CONFIGURATION_VERSION
+	bool "Increment config version from SMP set attribute"
 	help
 	  Will increment the configuration version attribute upon a
-	  successful SMP set parameter being executed, requires the
+	  successful SMP set attribute being executed, requires the
 	  config version attribute to be present.
 
 endif # MCUMGR_CMD_ATTRIBUTE_MGMT
@@ -239,33 +179,24 @@ config ATTR_LOAD_FEEDBACK
 	  load parameter commands are used which can then be retrieved
 	  to see if any attributes could not be set.
 
-if ATTR_LOAD_FEEDBACK
-
-config ATTRIBUTE_MGMT_FEEDBACK_FILE
-	string "File name used for feedback for SMP load function"
-	default "/ext/load_bt_feedback.txt"
-	depends on MCUMGR_CMD_ATTRIBUTE_MGMT
-	help
-	  This is the full path and file name for the feedback file
-	  which is output when the load parameter management command is
-	  used.
-
-config ATTR_SHELL_FEEDBACK_FILE
-	string "File name used for feedback for shell load function"
-	default "/ext/load_shell_feedback.txt"
-	depends on ATTR_SHELL
-	help
-	  This is the full path and file name for the feedback file
-	  which is output when the load parameter shell command is used.
-
-endif # ATTR_LOAD_FEEDBACK
+config ATTRIBUTE_FEEDBACK_FILE
+	string "File name used for feedback for load function"
+	depends on ATTR_LOAD_FEEDBACK
+	default "/lfs1/attr_feedback.txt"
 
 config ATTR_ENCRYPTED
 	bool "Encrypt attribute files"
-	depends on LCZ_PARAM_FILE_ENCRYPTED
+	depends on FSU_ENCRYPTED_FILES
 	help
 	  When enabled, attribute and attribute load files will be
 	  encrypted.
+
+config ATTR_FILE_ADDITIONAL_COMMENT_CHARS
+	int "Allow extra space in key-value pair file for comments"
+	default 320
+	help
+	  The attribute generator accounts generates the maximum file size
+	  based on the maximum length of each attribute.
 
 # Include logging configuration
 module = ATTR

include/attr_defs.h
@@ -66,25 +66,23 @@ enum attr_write_error {
 	ATTR_WRITE_ERROR_NUMERIC_TOO_LOW,
 	ATTR_WRITE_ERROR_NUMERIC_TOO_HIGH,
 	ATTR_WRITE_ERROR_STRING_TOO_MANY_CHARACTERS,
-	ATTR_WRITE_ERROR_PARAMETER_READ_ONLY,
+	ATTR_WRITE_ERROR_PARAMETER_NOT_WRITABLE,
 	ATTR_WRITE_ERROR_PARAMETER_UNKNOWN,
-	ATTR_WRITE_ERROR_PARAMETER_INVALID_LENGTH
+	ATTR_WRITE_ERROR_PARAMETER_INVALID_LENGTH,
+	ATTR_WRITE_ERROR_UNKNOWN,
+	ATTR_WRITE_ERROR_UNABLE_TO_PARSE_FILE,
+	ATTR_WRITE_ERROR_CODE_COUNT
 };
 
 enum attr_flags {
 	FLAGS_WRITABLE = 0x1,
 	FLAGS_READABLE = 0x2,
-	FLAGS_LOCKABLE = 0x4,
+	FLAGS_RESERVED = 0x4,
 	FLAGS_BROADCAST = 0x8,
 	FLAGS_SAVABLE = 0x10,
 	FLAGS_DEPRECATED = 0x20,
 	FLAGS_OBSCURE_IN_SHOW = 0x40,
 	FLAGS_HIDE_IN_SHOW = 0x80,
-	FLAGS_UNHIDE_UNOBSCURE_IN_SHOW_IF_UNLOCKED = 0x100,
-	FLAGS_OBSCURE_IN_DUMP = 0x200,
-	FLAGS_HIDE_IN_DUMP = 0x400,
-	FLAGS_UNHIDE_UNOBSCURE_IN_DUMP_IF_UNLOCKED = 0x800,
-	FLAGS_SHOW_ON_CHANGE = 0x1000,
 	FLAGS_NOTIFY_IF_VALUE_UNCHANGED = 0x2000
 };
 

commit eb54ef4439f63b7db7610e20e974d4192f673b1a

Fix config version increment
Bug 22934
Bug introduced in commit fe7d61535f8f2aed86c0028338de24833a215bb5.

Signed-off-by: Andrew Hedin <andrew.hedin@lairdconnect.com>

include/attr.h
@@ -644,11 +644,11 @@ bool attr_is_locked(void);
 int attr_force_save(void);
 
 /**
- * @brief Update (increment) configuration version attribute
+ * @brief Increment configuration version attribute
  *
  * @return int 0 on success, else negative errno.
  */
-int attr_update_config_version(void);
+int attr_increment_config_version(void);
 
 /**
  * @brief Save status callback

src/attr.c
@@ -1226,13 +1226,10 @@ int attr_force_save(void)
 #endif
 }
 
-int attr_update_config_version(void)
+int attr_increment_config_version(void)
 {
 #ifdef ATTR_ID_config_version
-	uint32_t config_version = attr_get_uint32(ATTR_ID_config_version, 0);
-
-	return attr_set_uint32(ATTR_ID_config_version,
-			       (uint32_t)config_version);
+	return attr_add_uint32(ATTR_ID_config_version, 1);
 #endif
 
 	return -EIO;

src/attribute_mgmt.c
@@ -540,9 +540,10 @@ static int set_parameter(struct mgmt_ctxt *ctxt)
 		r = -EINVAL;
 	}
 
-#ifdef CONFIG_ATTRIBUTE_MGMT_INCREMENT_CONFIG_VERSION
+#if defined(CONFIG_ATTRIBUTE_MGMT_INCREMENT_CONFIG_VERSION) &&                 \
+	defined(ATTR_ID_config_version)
 	if (r == 0 && modified == true) {
-		(void)attr_update_config_version();
+		(void)attr_add_uint32(ATTR_ID_config_version, 1);
 	}
 #endif
 